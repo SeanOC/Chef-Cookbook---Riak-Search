fe7d8aa3d8f7077cafe92a2d596ff033a8c9f7c4
Sean O'Connor <sean@saaspire.com>
2011-08-06 22:47:07 -0400

    Cookbook base

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Saaspire LLC"
+maintainer_email "sean@saaspire.com"
+license          "Apache 2.0"
+description      "Installs/Configures riaksearch"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
